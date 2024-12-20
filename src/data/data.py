90a6fa196f5c4cd40e70fadb733ddb07d4ae38e4
azbenoit <64805822+azbenoit@users.noreply.github.com>
2024-12-20 20:39:06 +0100

    fixed bug

--- src/data/data.py
+++ src/data/data.py
@@ -4,22 +4,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 
+CMU_PATH = "data/MovieSummaries"
+IMDB_PATH = "data/imdb_data"
+
 def load_metadata():
     metadata = pd.read_table("data/MovieSummaries/movie.metadata.tsv", header=None)
     metadata.columns = [
-        "Wikipedia_movie_ID",
+        "Wikipedia movie ID",
         "Freebase_movie_ID",
         "Movie_name",
         "Movie_release_date",
         "Movie_box_office_revenue",
         "Movie_runtime",
         "Movie_languages", 
-        "Movie_countxries",  
+        "Movie_countries",  
         "Movie_genres"     
     ]
     #more readable format
     metadata["Movie_genres"] = metadata["Movie_genres"].apply(lambda x: list(json.loads(x).values()))
-    metadata.loc[metadata['Wikipedia_movie_ID']==29666067,'Movie_release_date'] = '2010-12-02'
+    metadata.loc[metadata['Wikipedia movie ID']==29666067,'Movie_release_date'] = '2010-12-02'
     metadata["Movie_languages"] = metadata["Movie_languages"].apply(lambda x: list(json.loads(x).values()))
     metadata["Movie_countries"] = metadata["Movie_countries"].apply(lambda x: list(json.loads(x).values()))
     #fix country mappings
@@ -67,12 +70,32 @@ def load_metadata():
     )
     return metadata
 
+def load_characters():
+    characters = pd.read_table(f"{CMU_PATH}/character.metadata.tsv", header=None)
+
+    characters.columns = [
+    "Wikipedia movie ID",
+    "Freebase movie ID",
+    "Movie release date",
+    "Character name",
+    "Actor date of birth",
+    "Actor gender",
+    "Actor height (in meters)",
+    "Actor ethnicity (Freebase ID)",
+    "Actor name",
+    "Actor age at movie release",
+    "Freebase character/actor map ID",
+    "Freebase character ID",
+    "Freebase actor ID"
+    ]
+    return characters
+
 def load_summaries():
     with open("data/MovieSummaries/plot_summaries.txt", "r", encoding="utf-8") as f:
         file = f.readlines()
     data = [line.strip().split("\t", 1) for line in file]
-    summaries_df = pd.DataFrame(data, columns=["Wikipedia_movie_ID", "Movie_Summary"])
-    summaries_df['Wikipedia_movie_ID'] = summaries_df['Wikipedia_movie_ID'].astype("int")
+    summaries_df = pd.DataFrame(data, columns=["Wikipedia movie ID", "Movie_Summary"])
+    summaries_df['Wikipedia movie ID'] = summaries_df['Wikipedia movie ID'].astype("int")
     
     return summaries_df
 
